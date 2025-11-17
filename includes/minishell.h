829d4f21e408678fe1aeb712b9a564da8647709f
Talen Lavareda Pinto <TalenGitHub0@gmail.com>
2025-11-17 13:56:56 -0300

    feat: adding libft

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tlavared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 12:31:03 by tlavared          #+#    #+#             */
-/*   Updated: 2025/11/17 12:31:45 by tlavared         ###   ########.fr       */
+/*   Updated: 2025/11/17 13:36:23 by tlavared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 # define MINISHELL_H
 
 # include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <dirent.h>
+# include <string.h>
+# include <termios.h>
 
 #endif
