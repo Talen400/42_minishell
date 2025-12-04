aae4f97417d1c99b74d93956b399e2d76165089c
FelBenini <felbenini@gmail.com>
2025-12-04 13:24:17 -0300

    Feat: added pwd

---
+++ srcs/builtins/pwd.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbenini- <fbenini-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 19:15:07 by fbenini-          #+#    #+#             */
+/*   Updated: 2025/12/04 13:23:46 by fbenini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+#include "../../includes/builtins.h"
+#include "../../libft/libft.h"
+
+int	pwd(char **args, t_data *data)
+{
+	int		i;
+	char	*cwd;
+
+	i = 1;
+	(void)data;
+	while (args[i])
+		i++;
+	if (i > 1)
+	{
+		ft_putendl_fd("pwd: too many arguments", STDERR_FILENO);
+		return (1);
+	}
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		perror("pwd:");
+		return (1);
+	}
+	ft_putendl_fd(cwd, STDOUT_FILENO);
+	free(cwd);
+	return (0);
+}
