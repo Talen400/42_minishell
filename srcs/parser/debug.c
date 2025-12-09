1a790f1dbdafb3d5f73c4b04f53811bf6adf11d4
FelBenini <felbenini@gmail.com>
2025-12-09 16:02:19 -0300

    Fix: simple parenthesis not skipping properly on parser

--- srcs/parser/debug.c
+++ srcs/parser/debug.c
@@ -6,7 +6,7 @@
 /*   By: fbenini- <fbenini-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 20:39:03 by fbenini-          #+#    #+#             */
-/*   Updated: 2025/12/02 19:08:57 by fbenini-         ###   ########.fr       */
+/*   Updated: 2025/12/09 15:47:09 by fbenini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ static void	print_cmd_node(t_ast_node *node, int indent)
 	j = 0;
 	while (j++ < indent)
 		ft_printf("  ");
-	ft_printf("Command: %s [", node->u_data.cmd.cmd->processed);
+	ft_printf("Command: %s [", node->u_data.cmd.cmd->raw);
 	i = 0;
 	while (node->u_data.cmd.args[i++])
 	{
-		ft_printf("%d", node->u_data.cmd.args[i - 1]->processed);
+		ft_printf("%s", node->u_data.cmd.args[i - 1]->raw);
 		if (i - 1 < node->u_data.cmd.argc - 1)
 			ft_printf(", ");
 	}
@@ -37,7 +37,7 @@ static void	print_cmd_node(t_ast_node *node, int indent)
 			ft_printf("  ");
 		ft_printf("Redirect: %s %s\n",
 			node->u_data.cmd.redirects[i - 1]->type,
-			node->u_data.cmd.redirects[i - 1]->target->processed);
+			node->u_data.cmd.redirects[i - 1]->target->raw);
 	}
 }
 
