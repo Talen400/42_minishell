787f676bf004930ecd99a0310fa70c5a83471e55
Talen400 <talengithub0@gmail.com>
2025-12-23 16:53:37 -0300

    fix: return the version echo. Not noises in subshell

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,33 +6,53 @@
 /*   By: fbenini- <fbenini-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 12:06:04 by fbenini-          #+#    #+#             */
-/*   Updated: 2025/12/23 11:30:08 by tlavared         ###   ########.fr       */
+/*   Updated: 2025/12/23 16:52:59 by tlavared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include <unistd.h>
 
+static void	run_echo(char **args, int skip)
+{
+	int	len;
+	int	printed;
+	int	i;
+
+	len = 1;
+	while (args[len])
+		len++;
+	len--;
+	i = 1 + skip;
+	printed = 0;
+	while (args[i])
+	{
+		if (i <= len && printed > 0)
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		ft_putstr_fd(args[i++], STDOUT_FILENO);
+		printed++;
+	}
+}
+
 int	echo(char **args, t_data *data)
 {
 	int	i;
 	int	line_break;
 
+	i = 0;
 	(void)data;
-	i = 1;
-	line_break = 1;
-	if (args[i] && ft_strncmp(args[i], "-n", 3) == 0)
-	{
-		line_break = 0;
-		i++;
-	}
 	while (args[i])
-	{
-		ft_putstr_fd(args[i], STDOUT_FILENO);
-		if (args[i + 1])
-			ft_putchar_fd(' ', STDOUT_FILENO);
 		i++;
+	if (i < 2)
+	{
+		ft_putendl_fd("", STDOUT_FILENO);
+		return (SUCESS);
 	}
+	line_break = 1;
+	i = 1;
+	if (ft_strncmp(args[i++], "-n\0", 3) == 0)
+		line_break = 0;
+	run_echo(args, !line_break);
 	if (line_break)
 		ft_putchar_fd('\n', STDOUT_FILENO);
 	return (SUCESS);
